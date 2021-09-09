d3881d52874bd5857f5e0d2d62770b8c990f96cd
Thiago Almeida <thiagoduminellidealmeida@hotmail.com>
2021-09-09 16:55:53 -0300

    Create ex08.py

---
+++ ex08.py
@@ -0,0 +1,12 @@
+gabinete = []
+placa = []
+
+gabinete.insert(0, 'Gabinete cooler master')
+placa.insert(0, 'GeForce RTX 2080Ti')
+
+print ("Seu gabinete é: ",gabinete))
+print ("Sua placa de vídeo:", (placa))
+
+del gabinete[0]
+del placa[0]
+
