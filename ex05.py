1a0e6302008e9360430119fa7b08154c6d8f59b1
Thiago Almeida <thiagoduminellidealmeida@hotmail.com>
2021-09-09 16:55:46 -0300

    Create ex05.py

---
+++ ex05.py
@@ -0,0 +1,6 @@
+lista = [1, 2, 3, 4, 5]
+num = int(input("Digite um número para ver se está na lista:"))
+if num in lista:
+    print("Têm este numero na lista!")
+else:
+    print("Não têm esse número na lista!")
